50711bae4a8f5488080942d7682fc3159e1c5631
Hemil Desai <hemild@nvidia.com>
2025-08-15 08:27:10 -0700

    update recipe
    Signed-off-by: Hemil Desai <hemild@nvidia.com>

--- src/megatron/bridge/recipes/deepseek/deepseek_v3.py
+++ src/megatron/bridge/recipes/deepseek/deepseek_v3.py
@@ -96,8 +96,38 @@ def model_config(
         pass
 
     # Pipeline split for asymmetric stages as used in NeMo recipe
-    cfg.num_layers_in_first_pipeline_stage = 3
-    cfg.num_layers_in_last_pipeline_stage = 2
+    cfg.account_for_embedding_in_pipeline_split = False
+    cfg.account_for_loss_in_pipeline_split = False
+    cfg.num_layers_in_first_pipeline_stage = None
+    cfg.num_layers_in_last_pipeline_stage = None
+
+    # Performance optimization knobs
+    cfg.moe_permute_fusion = True
+    cfg.apply_rope_fusion = True
+
+    # Pipeline parallelism configs. We infer PP layout from the provided PP and VP size
+    map_pp_vp_to_layout = {
+        (1, 1): None,
+        (4, 1): [["embedding"] + ["decoder"] * 16, ["decoder"] * 16, ["decoder"] * 16, ["decoder"] * 13 + ["loss"]],
+        (8, 1): [["embedding"] + ["decoder"] * 8] + [["decoder"] * 8] * 6 + [["decoder"] * 5 + ["loss"]],
+        (4, 2): [["embedding"] + ["decoder"] * 8] + [["decoder"] * 8] * 6 + [["decoder"] * 5 + ["loss"]],
+        (16, 1): [["embedding"] + ["decoder"] * 4] + [["decoder"] * 4] * 14 + [["decoder", "loss"]],
+        (8, 2): [["embedding"] + ["decoder"] * 4] + [["decoder"] * 4] * 14 + [["decoder", "loss"]],
+        (4, 4): [["embedding"] + ["decoder"] * 4] + [["decoder"] * 4] * 14 + [["decoder", "loss"]],
+    }
+    pp_size = pipeline_parallelism or 1
+    vp_size = virtual_pipeline_parallelism or 1
+    if (pp_size, vp_size) not in map_pp_vp_to_layout:
+        raise ValueError(
+            f"Invalid PP and VP size: {pp_size} and {vp_size} to infer PP layout "
+            f"for DeepSeek V3. Known PP and VP combinations: {map_pp_vp_to_layout.keys()}"
+        )
+
+    layout = map_pp_vp_to_layout[(pp_size, vp_size)]
+
+    if layout is not None:
+        layout = list([list(x) for x in layout])  # yield all the elements
+    cfg.pipeline_model_parallel_layout = layout
 
     return cfg
 
