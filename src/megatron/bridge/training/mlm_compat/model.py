0e5ed557979fbc796762c47d57579b83336e3ef4
Maanu Grover <109391026+maanug-nv@users.noreply.github.com>
2025-08-22 12:50:29 -0700

    Refactor inference model load  (#447)
    * split function so model cfg can be modified
    
    Signed-off-by: Maanu Grover <maanug@nvidia.com>
    
    * enable vp
    
    Signed-off-by: Maanu Grover <maanug@nvidia.com>
    
    * fix load arg
    
    Signed-off-by: Maanu Grover <maanug@nvidia.com>
    
    * fix unit tests
    
    Signed-off-by: Maanu Grover <maanug@nvidia.com>
    
    * fix func test
    
    Signed-off-by: Maanu Grover <maanug@nvidia.com>
    
    * remove ddp and fsdp logic
    
    Signed-off-by: Maanu Grover <maanug@nvidia.com>
    
    * unit tests for mlm compat get model func
    
    Signed-off-by: Maanu Grover <maanug@nvidia.com>
    
    * fix arg
    
    Signed-off-by: Maanu Grover <maanug@nvidia.com>
    
    * change default
    
    Signed-off-by: Maanu Grover <maanug@nvidia.com>
    
    * add assert for missing mamba stack spec
    
    Signed-off-by: Maanu Grover <maanug@nvidia.com>
    
    ---------
    
    Signed-off-by: Maanu Grover <maanug@nvidia.com>

--- src/megatron/bridge/training/mlm_compat/model.py
+++ src/megatron/bridge/training/mlm_compat/model.py
@@ -15,6 +15,11 @@
 import argparse
 from typing import Optional
 
+import megatron.core.parallel_state as mpu
+import torch
+from megatron.core import tensor_parallel
+from megatron.core.enums import ModelType
+from megatron.core.fp8_utils import correct_amax_history_if_needed
 from megatron.core.models.gpt import GPTModel
 from megatron.core.models.gpt.gpt_layer_specs import (
     get_gpt_decoder_block_spec,
@@ -26,8 +31,10 @@ from megatron.core.models.gpt.heterogeneous.heterogeneous_layer_specs import (
     get_gpt_heterogeneous_layer_spec,
 )
 from megatron.core.models.mamba import MambaModel
-from megatron.core.transformer import ModuleSpec, TransformerConfig
+from megatron.core.transformer import MegatronModule, ModuleSpec, TransformerConfig
+from megatron.core.transformer.module import Float16Module
 from megatron.core.transformer.spec_utils import import_module
+from megatron.core.utils import get_model_config
 
 from megatron.bridge.training.mlm_compat.arguments import _transformer_config_from_args
 
@@ -132,6 +139,7 @@ def _mamba_provider(
     config: Optional[TransformerConfig] = None,
     pre_process: bool = True,
     post_process: bool = True,
+    vp_stage: Optional[int] = None,
 ) -> MambaModel:
     """Provide the MambaModel exactly as done by MLM using an argparse args object.
 
@@ -140,6 +148,7 @@ def _mamba_provider(
     if config is None:
         config = _transformer_config_from_args(args)
 
+    assert args.spec is not None, "You must provide a valid Mamba layer spec!"
     mamba_stack_spec = import_module(args.spec)
 
     model = MambaModel(
@@ -159,5 +168,85 @@ def _mamba_provider(
         rotary_percent=args.rotary_percent,
         rotary_base=args.rotary_base,
     )
+
+    return model
+
+
+def _get_model(
+    args: argparse.Namespace,
+    model_provider_func,
+    model_cfg: TransformerConfig,
+    model_type=ModelType.encoder_or_decoder,
+) -> list[MegatronModule]:
+    """Build the model vp stages and wrappers for inference. In the same style as MLM."""
+
+    # Build model.
+    def build_model():
+        if (
+            mpu.get_pipeline_model_parallel_world_size() > 1
+            and mpu.get_virtual_pipeline_model_parallel_world_size() is not None
+        ):
+            model = []
+            for i in range(mpu.get_virtual_pipeline_model_parallel_world_size()):
+                # Set pre_process and post_process only after virtual rank is set.
+                pre_process = mpu.is_pipeline_first_stage(ignore_virtual=False, vp_stage=i)
+                post_process = mpu.is_pipeline_last_stage(ignore_virtual=False, vp_stage=i)
+                this_model = model_provider_func(
+                    args, model_cfg, pre_process=pre_process, post_process=post_process, vp_stage=i
+                )
+                this_model.model_type = model_type
+                this_model.vp_stage = i
+                model.append(this_model)
+        else:
+            pre_process = mpu.is_pipeline_first_stage()
+            post_process = mpu.is_pipeline_last_stage()
+            model = model_provider_func(args, model_cfg, pre_process=pre_process, post_process=post_process)
+            model.model_type = model_type
+        return model
+
+    if args.init_model_with_meta_device:
+        with torch.device("meta"):
+            model = build_model()
+    else:
+        model = build_model()
+
+    if not isinstance(model, list):
+        model = [model]
+
+    # Set tensor model parallel attributes if not set.
+    # Only parameters that are already tensor model parallel have these
+    # attributes set for them. We should make sure the default attributes
+    # are set for all params so the optimizer can use them.
+    for model_module in model:
+        for param in model_module.parameters():
+            tensor_parallel.set_defaults_if_not_set_tensor_model_parallel_attributes(param)
+
+    # Print number of parameters.
+    num_parameters = sum([sum([p.nelement() for p in model_module.parameters()]) for model_module in model])
+    if mpu.get_data_parallel_rank() == 0 and mpu.get_context_parallel_rank() == 0:
+        print(
+            " > number of parameters on (tensor, pipeline) model parallel rank ({}, {}): {}".format(
+                mpu.get_tensor_model_parallel_rank(),
+                mpu.get_pipeline_model_parallel_rank(),
+                num_parameters,
+            ),
+            flush=True,
+        )
+
+    if not args.init_model_with_meta_device:
+        for model_module in model:
+            model_module.cuda(torch.cuda.current_device())
+
+    # Fp16 conversion.
+    if args.fp16 or args.bf16:
+        config = get_model_config(model[0])
+        model = [Float16Module(config, model_module) for model_module in model]
+
+    # Before TE2.x: The model_module.bfloat16()/model_module.half() above will call the inplace
+    #               copy of TE's Float8Tensor, which will write an unwanted value (amax calculated
+    #               from the current fp8 param) to its amax_history. The below function will correct
+    #               the amax_history back.
+    # After TE2.x: Below function is an empty function and does nothing.
+    correct_amax_history_if_needed(model)
 
     return model
