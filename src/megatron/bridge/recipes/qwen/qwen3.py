0b1ecf9c7a1528fbf0c460fac5f11023184f9ed2
Yu Yao <54727607+yaoyu-33@users.noreply.github.com>
2025-09-23 18:21:10 -0700

    Update src/megatron/bridge/recipes/qwen/qwen3.py
    Co-authored-by: Ananth Subramaniam <ansubramania@nvidia.com>
    Signed-off-by: Yu Yao <54727607+yaoyu-33@users.noreply.github.com>

--- src/megatron/bridge/recipes/qwen/qwen3.py
+++ src/megatron/bridge/recipes/qwen/qwen3.py
@@ -197,7 +197,6 @@ def _qwen3_common(
         model_cfg.recompute_method = "uniform"
         model_cfg.recompute_num_layers = 1
     
-    model_cfg.finalize()
 
     opt_cfg, scheulder_cfg = distributed_fused_adam_with_cosine_annealing(
         lr_warmup_iters=lr_warmup_iters,
